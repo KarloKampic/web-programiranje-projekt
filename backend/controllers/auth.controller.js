85ba9b961b48572df0bbcdce3f52be68a04bb3cc
Karlo Kampić <kkampic@mathos.hr>
2024-09-09 02:56:26 +0200

    1-backend gotov

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -59,14 +59,13 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    try {
-
-		const {username, password} = req.body;
-		const user = await User.findOne({username});
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
 		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
 		if (!user || !isPasswordCorrect) {
-			return res.status(400).json({error: "Invalid username or password"});
+			return res.status(400).json({ error: "Invalid username or password" });
 		}
 
 		generateTokenAndSetCookie(user._id, res);
@@ -81,7 +80,6 @@ export const login = async (req, res) => {
 			profileImg: user.profileImg,
 			coverImg: user.coverImg,
 		});
-
 	} catch (error) {
 		console.log("Error in login controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
@@ -90,22 +88,20 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
 	try {
-		res.cookie("jwt", "", {maxAge:0})
-		res.status(200).json({message: "Logged out successfully"});
-    }catch (error) {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
 		console.log("Error in logout controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
 
 export const getMe = async (req, res) => {
-
 	try {
 		const user = await User.findById(req.user._id).select("-password");
 		res.status(200).json(user);
 	} catch (error) {
 		console.log("Error in getMe controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
-	
 	}
 };
